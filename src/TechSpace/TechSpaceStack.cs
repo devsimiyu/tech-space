9c13aa90d469d0e8ead168cd5ae9db09eca4e39c
devsimiyu <dev.wrodgerss@gmail.com>
2024-02-10 10:48:21 +0300

    Initial commit

---
+++ src/TechSpace/TechSpaceStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace TechSpace
+{
+    public class TechSpaceStack : Stack
+    {
+        internal TechSpaceStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
